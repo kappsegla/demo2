1d3275e01ca79177a027fb3a650fb672e61ff673
Martin Blomberg <martin.blomberg@outlook.com>
2019-02-25 13:08:53 +0100

    Delete, Put

--- src/main/java/se/iths/martin/demo2/ProductController.java
+++ src/main/java/se/iths/martin/demo2/ProductController.java
@@ -20,7 +20,7 @@ public class ProductController {
 
     @GetMapping("/products/{id}")
     public Product getOne(@PathVariable Long id){
-        return repository.findById(id)
+                return repository.findById(id)
                 .orElseThrow( () -> new ProductException("No product with id " + id));
     }
 
@@ -29,6 +29,17 @@ public class ProductController {
         return repository.save(product);
     }
 
+    @DeleteMapping("/products/{id}")
+    public void delete(@PathVariable Long id){
+        repository.deleteById(id);
+    }
 
-
+    @PutMapping("/products/{id}")
+    public Product change(@RequestBody Product product, @PathVariable Long id){
+        return repository.findById(id).map(storedProduct -> {
+                    storedProduct.setProductName(product.getProductName());
+                    storedProduct.setDescription(product.getDescription());
+                    return repository.save(storedProduct);
+                }).orElseThrow( () -> new ProductException("No product with id " + id));
+    }
 }
